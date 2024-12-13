f2ea114b34946974706ed0458ad54b46035620ed
capitalappraisaldata <112714066+capitalappraisaldata@users.noreply.github.com>
2024-12-13 15:49:37 +0100

    borramos json

--- utils/loadGeoData.js
+++ utils/loadGeoData.js
@@ -1,47 +1,17 @@
-// const fs = require('fs');
-// const path = require('path');
-
-// // Cargar datos de ciudades
-// const cities = fs
-//   .readFileSync(path.join(__dirname, '../data/cities15000.txt'), 'utf8')
-//   .split('\n')
-//   .map((line) => {
-//     const parts = line.split('\t');
-//     return {
-//       name: parts[1],
-//       lat: parseFloat(parts[4]),
-//       lng: parseFloat(parts[5]),
-//       country_code: parts[8],
-//     };
-//   });
-
-// // Cargar datos de países
-// const countries = fs
-//   .readFileSync(path.join(__dirname, '../data/countryInfo.txt'), 'utf8')
-//   .split('\n')
-//   .filter((line) => line && !line.startsWith('#'))
-//   .map((line) => {
-//     const parts = line.split('\t');
-//     return {
-//       code: parts[0],
-//       name: parts[4],
-//     };
-//   });
-
-// console.log("Ciudades cargadas:", cities.slice(0, 5)); // Muestra las primeras 5 ciudades
-
-
-// module.exports = { cities, countries };
 
 // HEMOS MODIFICADO EL CODIGO PARA COGER SOLO ciudades por encima de los 100.000 habitantes, antes era 15000
 
 const fs = require('fs');
 const path = require('path');
 
+const citiesFilePath = path.resolve(__dirname, '..', 'data', 'cities15000.txt');
+const countryFilePath = path.resolve(__dirname, '..', 'data', 'countryInfo.txt');
+
+
 // Cargar datos de ciudades
 const cities = fs
-
-  .readFileSync(path.join(__dirname, '..', 'data', 'cities15000.txt'), 'utf8')
+  .readFileSync(citiesFilePath, 'utf8')
+  // .readFileSync(path.join(__dirname, '..', 'data', 'cities15000.txt'), 'utf8')
   // .readFileSync(path.join(__dirname, '../data/cities15000.txt'), 'utf8')
   .split('\n')
   .map((line) => {
@@ -58,7 +28,8 @@ const cities = fs
 
 // Cargar datos de países
 const countries = fs
-  .readFileSync(path.join(__dirname, '..','data','countryInfo.txt'), 'utf8')
+  .readFileSync(countryFilePath, 'utf8')
+  // .readFileSync(path.join(__dirname, '..','data','countryInfo.txt'), 'utf8')
   // .readFileSync(path.join(__dirname, '../data/countryInfo.txt'), 'utf8')
   .split('\n')
   .filter((line) => line && !line.startsWith('#'))
